95b2d0b575198f08813cf22adb3a3d8ebc0fd603
Rimjhim Prakash <91970428+prakash-rimjhim@users.noreply.github.com>
2022-09-17 15:20:15 +0530

    Update Prefix Sum Problem.py

--- Prefix Sum Problem.py
+++ Prefix Sum Problem.py
@@ -12,6 +12,23 @@ class NumArray:
 
     def sumRange(self, left: int, right: int) -> int:
         return self.sums[right + 1] - self.sums[left]
+    
+    
+////////////Second Method :
+    class NumArray:
+
+    def __init__(self, nums: List[int]):
+        self.nums = nums
+        for i in range(1,len(nums)):
+            nums[i] = nums[i] + nums[i-1]
+        
+              
+
+    def sumRange(self, left: int, right: int) -> int:
+        if left ==0:
+            return self.nums[right]
+        else:
+            return self.nums[right] - self.nums[left-1]
         
 
 Q. 2 https://leetcode.com/problems/range-sum-query-mutable/
